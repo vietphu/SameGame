485813c0ecf034aa7d6880f921245b5188cd8230
Eno Compton 4 <eno4@ecom.com>
2012-05-13 22:12:20 +0900

    Initial Commit

---
+++ SameGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SameGame
+//
+//  Created by Eno Compton on 5/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
